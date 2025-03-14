b6eb85e528ed034b1335a9d4d096ccda1b0a5945
Abir Hossen <abir.cse4@gmail.com>
2025-03-14 14:27:16 +0600

    SSL Commerz

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Main = () => {
+    return (
+        <div>
+            <p className='text-5xl'>Hello </p>
+        </div>
+    );
+};
+
+export default Main;
